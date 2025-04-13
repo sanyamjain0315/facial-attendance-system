a4c2f11cf5d11c9332cbf0d940f4a13f0a625e45
sanyamjain0315 <sanyamjain0315@gmail.com>
2025-04-14 02:30:24 +0530

    Created working face embeddings generator class
    updated dockerfile and requirements

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,10 @@ WORKDIR /app
 
 COPY /requirements.txt .
 
+RUN apt-get clean && apt-get -y update && apt-get install -y build-essential cmake ffmpeg libsm6 libxext6 libopenblas-dev liblapack-dev libopenblas-dev liblapack-dev
+
 RUN pip install --no-cache-dir -r requirements.txt
 
-COPY . .
+# COPY . .
 
 CMD ["bash"]
--- facial_recognizer.py
+++ facial_recognizer.py
@@ -1,50 +1,56 @@
+import face_recognition
 import cv2
 import matplotlib.pyplot as plt
-from imgbeddings import imgbeddings
-from PIL import Image
+import numpy as np
+from PIL import Image, ImageDraw
 
 
 class FacialRecognizer:
-    def __init__(self, face_model_path="haarcascade_frontalface_default.xml"):
-        self.haar_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-        self.image_embedder = imgbeddings()
+    def __init__(self):
+        pass
     
-    def extract_faces(self, image):
-        gray_img = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        faces = self.haar_cascade.detectMultiScale(gray_img, scaleFactor=1.05, minNeighbors=2, minSize=(100, 100))
-
-        # for each face detected
-        cropped_faces = []
-        for x, y, w, h in faces:
-            # crop the image to select only the face
-            cropped_faces.append(image[y : y + h, x : x + w])
-            
-            # loading the target image path into target_file_name variable
-            # target_file_name = '<INSERT YOUR OUTPUT FACE IMAGE NAME HERE> for eg-> X2.jpg'
-            # cv2.imwrite(target_file_name, cropped_image)
-        return cropped_faces
+    def extract_boxes(self, image, model="hog"):
+        return face_recognition.face_locations(image,model=model)
     
-    def calculate_embeddings(self, images):
-        # img = [Image.open(image) for image in images]
-        embeddings = [self.image_embedder.to_embeddings(image)[0] for image in images]
-        # embedding = self.image_embedder.to_embeddings(img)[0]
-        return embeddings
+    def get_cropped_faces(self, image, boxes=None):
+        if boxes == None:
+            boxes = self.extract_boxes(image)
+        found_faces = []
+        for (top, right, bottom, left) in boxes:
+                found_faces.append(image[top:bottom, left:right])    
+        return found_faces
+    
+    def calculate_embeddings(self, image, boxes=None):
+        return face_recognition.face_encodings(image, boxes)
+    
+    def _read_image(self, path):
+        image = cv2.imread(path)
+        rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        return rgb_image
     
 if __name__=="__main__":
-    facial_recognizer = FacialRecognizer()
-    image = cv2.imread("input_images/input.jpg")
-    extracted_faces = facial_recognizer.extract_faces(image=image)
-    faces_embeddings = facial_recognizer.calculate_embeddings(extracted_faces)
-    print(faces_embeddings)
-
-    # plt.figure(figsize=(3 * len(extracted_faces), 3))  # width scales with number of faces
-
-    # for i, img in enumerate(extracted_faces):
-    #     plt.subplot(1, len(extracted_faces), i+1)
-    #     plt.imshow(img, cmap='gray')
-    #     plt.axis('off')
-
-    # plt.tight_layout()
-    # plt.savefig('my_figure.png', dpi=300)  # Lower DPI is often enough if figure is larger
-    # plt.show()
-
+    fc = FacialRecognizer()
+    
+    # Get images
+    image = fc._read_image("input_images/input.jpg")
+    
+    boxes = fc.extract_boxes(image)
+    
+    # Extract all faces in all images
+    extracted_faces = fc.get_cropped_faces(image, boxes)
+    plt.figure(figsize=(3 * len(extracted_faces), 3))  # width scales with number of faces
+    for i, img in enumerate(extracted_faces):
+        plt.subplot(1, len(extracted_faces), i+1)
+        plt.imshow(img, cmap='gray')
+        plt.axis('off')
+    plt.tight_layout()
+    plt.savefig('output.png', dpi=300)  # Lower DPI is often enough if figure is larger
+    plt.show()
+    
+    # Choose the images to save
+    boxes = boxes[:4]
+    
+    # Create embeddings
+    embeddings = fc.calculate_embeddings(image, boxes)
+    # print(embeddings)
+    # print(len(embeddings))
